ffc483e0e7a6707e86210dae84c865af14c68e15
Arch Leaders <archleadership28@gmail.com>
2023-08-08 02:47:20 -0700

    Clenup test project (main)

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -5,7 +5,11 @@
 
 int main(int argc, char** argv) {
   std::cout << "[c++] Init Testing" << std::endl;
-  const auto data = file::util::ReadAllBytes(argv[1]);
-  aal::AudioResources bars{{data.data(), data.size()}};
-  std::cout << bars.GetResource(argv[2]).value().name << std::endl;
+  if (argc >= 2) {
+    const auto data = file::util::ReadAllBytes(argv[1]);
+    aal::AudioResources bars{{data.data(), data.size()}};
+    std::cout << bars.GetResource(argv[2]).value().name << std::endl;
+  } else {
+    std::cout << "[c++] Invalid arguments" << std::endl;
+  }
 }
